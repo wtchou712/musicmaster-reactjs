e5f66d19e75157ecadce9b89c5041944f7f013f3
William Chou <wtchou712@gmail.com>
2017-10-01 23:08:37 -0700

    spotify api added

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,23 @@ class App extends Component {
 
 	search(){
 		console.log('this.state', this.state);
+		const BASE_URL = 'https://api.spotify.com/v1/search?';
+		const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+		console.log('FETCH_URL', FETCH_URL);
+		var accessToken = 'BQDhaF2N6pS5fASCIZurPEdngda8z0Ci5G3P7xDWlYDbxvUPpdqf8g5vmxlzIwGWXhP9xjhZ7Aix6yvForPgL3sdab8-6O7SaYRXmU3WtfHRu9p8htrJAZ1K7Y4fCpCV3vyKWwSZTw4SU1_wDcHqz0aEyZyDVDQupw&refresh_token=AQCPPXfNhnK0XK4EAB9ydRI0x_K7lRBI0SExU9fwWvg6BQPbOuY-MUHsAYP8Ua_yBdQxQH6cjoNCnrRm8cI_e352KJEMRC44vJ1gycjwm1ce2iDBNksa2QqvcljpiV0PJd4'
+
+		var myOptions = {
+			method: 'GET', 
+			headers: {
+				'Authorization': 'Bearer ' + accessToken
+			},
+			mode: 'cors', 
+			cache: 'default'
+		};
+
+		fetch(FETCH_URL, myOptions)
+			.then(response=>response.json())
+			.then(json=>console.log(json))
 	}
 
 	render(){
