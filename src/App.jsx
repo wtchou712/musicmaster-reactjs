27d398c1047f9cd1532b9311bdcd1a555cff5ecb
William Chou <wtchou712@gmail.com>
2017-10-02 22:54:10 -0700

    get access token using refresh token - in progress

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,35 @@ class App extends Component {
 	constructor(props) {
 		super(props); 
 		this.state = {
-			query: ''
+			query: '',
+			artist: null
 		}
 	}
 
+	getAccessToken(){
+		//incomplete, will need updating
+		var refreshToken = 'AQBxgVubvF_fTDoAbg0BAVc1_EXCihsCsc0KsvAhkGVyeEO4z3gVLltRWp-PYUU66iuvvtkksf3sF2L2nKBotfACgzI6UufuKfgnBRSQgNEFmNkroVWsfvXu2s-4_qRCXGY';
+		const BASE_URL = 'https://accounts.spotify.com/api/token?';
+		const FETCH_URL =`${BASE_URL}grant_type=refresh_token&refresh_token=${refreshToken}`;
+
+		console.log('FETCH_URL', FETCH_URL);
+
+		fetch(FETCH_URL, {
+			method: 'POST', 
+			headers: {
+				'Authorization': 'Basic ' + refreshToken
+			}, 
+			mode: 'no-cors', 
+			cache: 'default'
+		})
+		.then(response=>console.log(response));
+	}
+
 	search(){
-		console.log('this.state', this.state);
 		const BASE_URL = 'https://api.spotify.com/v1/search?';
 		const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-		console.log('FETCH_URL', FETCH_URL);
-		var accessToken = 'BQDhaF2N6pS5fASCIZurPEdngda8z0Ci5G3P7xDWlYDbxvUPpdqf8g5vmxlzIwGWXhP9xjhZ7Aix6yvForPgL3sdab8-6O7SaYRXmU3WtfHRu9p8htrJAZ1K7Y4fCpCV3vyKWwSZTw4SU1_wDcHqz0aEyZyDVDQupw&refresh_token=AQCPPXfNhnK0XK4EAB9ydRI0x_K7lRBI0SExU9fwWvg6BQPbOuY-MUHsAYP8Ua_yBdQxQH6cjoNCnrRm8cI_e352KJEMRC44vJ1gycjwm1ce2iDBNksa2QqvcljpiV0PJd4'
 
+		var accessToken = 'BQDv92oOrhMSv0Ya5aLXEbh8eOYXBiQ-Ph1SNa9tVfIMG2AbGZXwTU6vA-Qvm0pHmoqFdIGxHMjigjaNaQFE4HgCXN_LPj8UckS3VJ4Tv18haEiNh9K6uEhuXHU2Evxn46V7RbJvwPhfHy7qffvAOQB9PkNGMQpm-g&refresh_token=AQADEYGziA0hPvhAmRmd641gVK9Z0DIeo9CxkKr5CKwKqGKPUct_FIjNke1mYxOlhriz7urqGNvoLc0SER-RAOaX0xrXKmW7j6qN6CBIvj3cvSCxMqHolDuKQi8wHj99Ono';
 		var myOptions = {
 			method: 'GET', 
 			headers: {
@@ -28,7 +46,11 @@ class App extends Component {
 
 		fetch(FETCH_URL, myOptions)
 			.then(response=>response.json())
-			.then(json=>console.log(json))
+			.then(json=> {
+				const artist = json.artists.items[0];
+				console.log('artist', artist);
+				this.setState({artist});
+			})
 	}
 
 	render(){
